d5fa7f1706f9bda428c27495e88c903c293f5ab7
ripon <riponahmed2201@gmail.com>
2021-04-26 03:54:12 +0600

    advance validation complete

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class APIController extends Controller
 {
@@ -25,24 +26,45 @@ class APIController extends Controller
 
             // Simple Post API Validation
 
-            //Check User Details
-            if(empty($userData['name']) || empty($userData['email']) || empty($userData['password'])){
-                $error_message = "Please enter complete user details!";
-            }
+            // //Check User Details
+            // if(empty($userData['name']) || empty($userData['email']) || empty($userData['password'])){
+            //     $error_message = "Please enter complete user details!";
+            // }
 
-            //Check if email valid
-            if (!filter_var($userData['email'], FILTER_VALIDATE_EMAIL)) {
-                $error_message = "Please enter valid Email!";
-            }
+            // //Check if email valid
+            // if (!filter_var($userData['email'], FILTER_VALIDATE_EMAIL)) {
+            //     $error_message = "Please enter valid Email!";
+            // }
 
-            //Check if User Email Already Exists
-            $userCount = User::where('email',$userData['email'])->count();
-            if ($userCount>0) {
-                $error_message = "Email already exists!";
-            }
+            // //Check if User Email Already Exists
+            // $userCount = User::where('email',$userData['email'])->count();
+            // if ($userCount>0) {
+            //     $error_message = "Email already exists!";
+            // }
 
-            if (isset($error_message) && !empty($error_message)) {
-                return response()->json(["status" => false, "message" => $error_message],422);
+            // if (isset($error_message) && !empty($error_message)) {
+            //     return response()->json(["status" => false, "message" => $error_message],422);
+            // }
+
+            // Advanced Post API Validations
+
+            $rules =[
+                'name' => 'required|regex:/^[\pL\s\-]+$/u',
+                'email' => 'required|email|unique:users',
+                'password' => 'required'
+            ];
+
+            $customMessage = [
+                'name.required' => 'Name is required',
+                'email.required' => 'Email is required',
+                'email.email' => 'Valid Email is required',
+                'email.unique' => 'Email already exists in database',
+                'password.required' => 'Password is required',
+            ];
+
+            $validators = Validator::make($userData, $rules,$customMessage);
+            if ($validators->fails()) {
+               return response()->json($validators->errors(),422);
             }
 
             $user = new User();
